2ae8488db67a5656edeccc1da946a20c1209327f
Kyle Cox <kylecox213@gmail.com>
2019-10-10 23:34:33 -0400

    completed customer.js model; all fields and associations

---
+++ models/customer.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, DataTypes) {
+    let Customer = sequelize.define("Customer", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        unit: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        state: {
+            type: DataTypes.STRING(2),
+            allowNull: false
+        },
+        zipCode: {
+            type: DataTypes.INTEGER(5),
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    Customer.associate = function(models) {
+        Customer.belongsToMany(models.Customer, { through: 'Relationships' })
+        Customer.belongsToMany(models.Vehicle, { through: 'Relationships' })
+        Customer.belongsToMany(models.Transaction, { through: 'Relationships' });
+    }
+
+    return Customer;
+};
