50ee3e2231f2cb2717ac4148eb4ae097158a656e
dvavs <vavradylan@gmail.com>
2019-10-16 00:53:19 -0400

    step two of the hopeful fix for page routing in react

---
+++ client/src/pages/Browse.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col, Row, Container } from "../../components/Grid";
+import Jumbotron from "../../components/Jumbotron";
+
+function Browse() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1>Browsing Page</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Browse;
