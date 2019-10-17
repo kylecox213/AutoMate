266aa7b290d14c65999da68c7517739173d84226
Kyle Cox <kylecox213@gmail.com>
2019-10-17 02:01:54 -0400

    remodification of form fields for customer and vehicle pages; added other fields discussed in business requirements

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import {Input, FormBtn } from "../components/Form";
+import {Input, FormBtn, EditBtn } from "../components/Form";
 
 class Homepage extends React.Component {
 
   state = {
     addType: "",
     firstName: "",
+    middleName: "",
     lastName: "",
     address: "",
     unit: "",
     city: "",
     state: "",
     zip: "",
-    phone: ""
+    phone: "",
+    email: "",
+    make: "",
+    model: "",
+    year: "",
+    color: "",
+    plateNumber: "",
+    vin: ""
   }
 
   handleInputChange = event => {
@@ -29,7 +37,7 @@ class Homepage extends React.Component {
       <Container fluid>
         <Row>
           <Col size="md-12">
-            <h1 style={{ textAlign: "center", margin: "50px auto" }}>New Customer</h1>
+            <h1 style={{ textAlign: "center", margin: "10px auto" }}>New Customer</h1>
           </Col>
         </Row>
         <Row>
@@ -37,7 +45,7 @@ class Homepage extends React.Component {
           <Col size="md-10" style={{ marginBottom: "50px" }}>
             <form>
               <div className="form-row" style={{ marginTop: "30px" }}>
-              <Col size="sm-6">
+              <Col size="sm-4">
                   <Input
                     type="input"
                     name="firstName"
@@ -47,7 +55,17 @@ class Homepage extends React.Component {
                     onChange={this.handleInputChange}
                   />
                 </Col>
-                <Col size="sm-6">
+                <Col size="sm-4">
+                  <Input
+                    type="input"
+                    name="firstName"
+                    id="inputFirstName"
+                    label="First Name"
+                    placeholder="First Name"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-4">
                   <Input
                     type="input"
                     name="lastName"
@@ -91,7 +109,7 @@ class Homepage extends React.Component {
                     onChange={this.handleInputChange}
                   />
                 </Col>
-                <Col size="sm-2">
+                <Col size="sm-1">
                   <Input
                     type="input"
                     name="state"
@@ -101,7 +119,7 @@ class Homepage extends React.Component {
                     onChange={this.handleInputChange}
                   />
                 </Col>
-                <Col size="sm-2">
+                <Col size="sm-1">
                   <Input
                     type="input"
                     name="zip"
@@ -111,13 +129,87 @@ class Homepage extends React.Component {
                     onChange={this.handleInputChange}
                   />
                 </Col>
-                <Col size="sm-4">
+                <Col size="sm-2">
                   <Input
                     type="input"
                     name="phone"
                     id="inputPhone"
                     label="Phone Number"
                     placeholder="Phone Number"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-4">
+                  <Input
+                    type="input"
+                    name="email"
+                    id="inputEmail"
+                    label="Email"
+                    placeholder="Email"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+              </div>
+            </form>
+            <form>
+              <div className="form-row" style={{ marginTop: "70px" }}>
+              <Col size="sm-2">
+                  <Input
+                    type="input"
+                    name="make"
+                    id="inputMake"
+                    label="Make"
+                    placeholder="Make"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-2">
+                  <Input
+                    type="input"
+                    name="model"
+                    id="inputModel"
+                    label="Model"
+                    placeholder="Model"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-1">
+                  <Input
+                    type="input"
+                    name="year"
+                    id="inputYear"
+                    label="Year"
+                    placeholder="Year"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-2">
+                  <Input
+                    type="input"
+                    name="color"
+                    id="inputColor"
+                    label="Color"
+                    placeholder="Color"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-2">
+                  <Input
+                    type="input"
+                    name="plateNumber"
+                    id="inputPlateNumber"
+                    label="Plate Number"
+                    placeholder="Plate Number"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="sm-3">
+                  <Input
+                    type="input"
+                    name="vin"
+                    id="inputVin"
+                    label="VIN"
+                    placeholder="VIN"
                     onChange={this.handleInputChange}
                   />
                 </Col>
