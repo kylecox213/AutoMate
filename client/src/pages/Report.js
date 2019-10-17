50a78d4ca2bf08ea7a08b006b58d1c581f79c876
Kyle Cox <kylecox213@gmail.com>
2019-10-17 19:43:33 -0400

    more formatting for report page; imported radio buttons

--- client/src/pages/Report.js
+++ client/src/pages/Report.js
@@ -1,34 +1,14 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
-import { Input, FormBtn, EditBtn } from "../components/Form";
-
+import { Input, FormBtn, EditBtn, Radio } from "../components/Form";
 class Report extends React.Component {
 
-  tate = {
-    addType: "",
-    firstName: "",
-    middleName: "",
-    lastName: "",
-    address: "",
-    unit: "",
-    city: "",
-    state: "",
-    zip: "",
-    phone: "",
-    email: "",
-    make: "",
-    model: "",
-    year: "",
-    color: "",
-    plateNumber: "",
-    odometer: "",
-    vin: "",
-    date: "",
-    parts: "",
-    labor: "",
-    total: "",
-    tax: "",
-    grandTotal: ""
+  state = {
+    startDate: "",
+    endDate: "",
+    salesResolution: "",
+    revenueResolution: "",
+    blob: ""
   }
 
   handleInputChange = event => {
@@ -40,7 +20,67 @@ class Report extends React.Component {
 
   render() {
     return (
-       ""
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <h1 style={{ textAlign: "center", margin: "10px auto" }}>Generate Report</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-1" />
+          <Col size="md-10" style={{ marginBottom: "50px" }}>
+            <form>
+              <h3 style={{ textAlign: "left", margin: "10px auto" }}>Date Range:</h3>
+              <Row>
+                <Col size="md-2" style={{ float: "left", marginBottom: "50px" }}>
+                  <Input
+                    type="input"
+                    name="startDate"
+                    id="inputStartDate"
+                    label="Start Date"
+                    placeholder="Start Date"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="md-2" style={{ float: "left", marginBottom: "50px" }}>
+                  <Input
+                    type="input"
+                    name="endDate"
+                    id="inputEndDate"
+                    label="End Date"
+                    placeholder="End Date"
+                    onChange={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="md-8" />
+              </Row>
+              <h3 style={{ textAlign: "left", margin: "10px auto" }}>Data Fields:</h3>
+              <Row>
+                <Col size="md-2" style={{ float: "left", marginBottom: "50px" }}>
+                  <Radio
+                    name="reportType"
+                    id="reportRadio"
+                    value="salesByDate"
+                    label="By Date"
+                    onClick={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="md-2" style={{ float: "left", marginBottom: "50px" }}>
+                  <Radio
+                    name="reportType"
+                    id="reportRadio"
+                    value="revenueByDate"
+                    label="By Date"
+                    onClick={this.handleInputChange}
+                  />
+                </Col>
+                <Col size="md-8" />
+              </Row>
+            </form>
+          </Col>
+          <Col size="md-1" />
+        </Row>
+      </Container>
     )
   };
 }
